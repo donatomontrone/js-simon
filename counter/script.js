38e3a1b0801ad96564e3da3b78b617d5e34aa18b
donatomontrone <donato.montrone@outlook.com>
2022-12-07 19:17:39 +0100

    fix backgroud

--- counter/script.js
+++ counter/script.js
@@ -1,17 +1,24 @@
-let counter = 0;
 buttonElement = document.querySelector('button');
+let counter = 0;
+let stopCount = false;
 
 buttonElement.addEventListener(('click'), function(){
-    if (counter < 10) {
-        counter++;
-        buttonElement.innerHTML = `Play (${counter})`;
-        buttonElement.classList.remove('bg-danger');
-    } else {
-        buttonElement.classList.add('bg-danger');
-        alert('Attendi')
-        setTimeout(()=> {
-            counter = 0;
+    buttonElement.innerHTML = `Play (${counter})`;
+    if (!stopCount) {
+        if (counter < 10) {
+            counter++;
             buttonElement.innerHTML = `Play (${counter})`;
-        }, 3 * 1000); 
+        } else {
+            setTimeout(()=> {
+                counter = 0;
+                buttonElement.classList.remove('btn-danger');
+                buttonElement.innerHTML = `Play (${counter})`;
+                stopCount = false;
+            }, 3 * 1000);
+        }
+    }
+
+    if (counter == 10) {
+        buttonElement.classList.add('btn-danger');
     }
 })
