4a7567e5f9b2af210b3c5a8211627cd727396963
donatomontrone <donato.montrone@outlook.com>
2022-12-07 18:23:35 +0100

    add counter

---
+++ counter/script.js
@@ -0,0 +1,20 @@
+let counter = 0;
+let stopTime = false;
+
+buttonElement = document.querySelector('button');
+
+
+buttonElement.addEventListener(('click'), function(){
+    if (counter < 10) {
+        counter++;
+        buttonElement.innerHTML = `Play (${counter})`;
+        buttonElement.classList.remove('bg-danger');
+    } else {
+        buttonElement.classList.add('bg-danger');
+        alert('Attendi')
+        setTimeout(()=> {
+            counter = 0;
+            buttonElement.innerHTML = `Play (${counter})`;
+        }, 3 * 1000); 
+    }
+})
